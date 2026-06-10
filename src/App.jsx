17ac38801688ccad7e5d47bb594b047b9a869d74
bt3762334-commits <bt3762334@gmail.com>
2026-06-10 23:42:42 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import Lessons from "./pages/Lessons";
+import Lectures from "./pages/Lectures";
+import Achievements from "./pages/Achievements";
+import Settings from "./pages/Settings";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tasks" element={<Tasks />} />
+      <Route path="/lessons" element={<Lessons />} />
+      <Route path="/lectures" element={<Lectures />} />
+      <Route path="/achievements" element={<Achievements />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
+  );
+}
+
+export default App;
