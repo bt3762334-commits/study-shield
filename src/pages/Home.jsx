b5ed0056d64e1c2aa8e1f5fc26e3403a50a195a9
bt3762334-commits <bt3762334@gmail.com>
2026-06-11 00:19:49 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
 import MainLayout from "../layout/MainLayout";
+import verse from "../data/quranVerse";
+
+import StatCard from "../components/ui/StatCard";
+import ProgressCard from "../components/ui/ProgressCard";
 
 export default function Home() {
   return (
     <MainLayout>
-      <h2>الصفحة الرئيسية</h2>
+      <section className="hero">
+        <h1>🛡️ Study Shield</h1>
+
+        <p>
+          نظم دراستك ومهامك اليومية وحقق أهدافك خطوة بخطوة
+        </p>
+      </section>
+
+      <section className="verse-card">
+        <h2>{verse.text}</h2>
+        <span>{verse.source}</span>
+      </section>
+
+      <section className="stats-grid">
+        <StatCard title="المهام اليوم" value="8" />
+        <StatCard title="الدروس القادمة" value="3" />
+        <StatCard title="المحاضرات" value="2" />
+        <StatCard title="الإنجاز" value="80%" />
+      </section>
+
+      <ProgressCard />
     </MainLayout>
   );
 }
