a40a4e53663cc3390b447b11920fe43202b4e2b6
bt3762334-commits <bt3762334@gmail.com>
2026-06-11 00:55:20 +0300

    Update AchievementPreview.jsx

--- src/components/ui/AchievementPreview.jsx
+++ src/components/ui/AchievementPreview.jsx
@@ -1,16 +1,43 @@
+import {
+  getTasks
+} from "../../services/taskStorage";
+
 export default function AchievementPreview() {
+  const completed =
+    getTasks().filter(
+      task => task.completed
+    ).length;
+
+  let badge = "🌱";
+  let title = "بداية الطريق";
+
+  if (completed >= 5) {
+    badge = "🥉";
+    title = "منجز مبتدئ";
+  }
+
+  if (completed >= 10) {
+    badge = "🥈";
+    title = "منجز محترف";
+  }
+
+  if (completed >= 20) {
+    badge = "🥇";
+    title = "أسطورة الإنجاز";
+  }
+
   return (
     <div className="dashboard-card achievement-preview">
       <h3>آخر وسام</h3>
 
       <div className="badge-preview">
-        🛡️
+        {badge}
       </div>
 
-      <h2>First Step</h2>
+      <h2>{title}</h2>
 
       <p>
-        أكملت أول مهمة بنجاح
+        أنجزت {completed} مهمة
       </p>
     </div>
   );
