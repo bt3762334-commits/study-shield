e09547aac65e65b509db47fdf27afd4661ab77e7
bt3762334-commits <bt3762334@gmail.com>
2026-06-11 00:54:08 +0300

    Update UpcomingTasks.jsx

--- src/components/ui/UpcomingTasks.jsx
+++ src/components/ui/UpcomingTasks.jsx
@@ -1,21 +1,28 @@
 export default function UpcomingTasks() {
-const [stats, setStats] = useState({
-  tasks: 0,
-  lessons: 0,
-  lectures: 0,
-  progress: 0
-});
-
-useEffect(() => {
-  setStats(getDashboardStats());
-}, []);
+  const tasks = [
+    {
+      title: "مراجعة React",
+      time: "6:00 مساءً"
+    },
+    {
+      title: "حل Assignment",
+      time: "8:00 مساءً"
+    },
+    {
+      title: "قراءة فصل Database",
+      time: "10:00 مساءً"
+    }
+  ];
 
   return (
     <div className="dashboard-card">
       <h3>المهام القادمة</h3>
 
       {tasks.map((task, index) => (
-        <div className="task-item" key={index}>
+        <div
+          className="task-item"
+          key={index}
+        >
           <div>
             <h4>{task.title}</h4>
           </div>
