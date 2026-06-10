0d27fc95a97f06a9dad5b71106f0d5b8177d126a
bt3762334-commits <bt3762334@gmail.com>
2026-06-10 23:49:35 +0300

    Update Achievements.jsx

--- Achievements.jsx
+++ Achievements.jsx
@@ -1,3 +1,9 @@
+import MainLayout from "../layout/MainLayout";
+
 export default function Achievements() {
-  return <h1>Achievements Page</h1>;
+  return (
+    <MainLayout>
+      <h2>الأوسمة</h2>
+    </MainLayout>
+  );
 }
