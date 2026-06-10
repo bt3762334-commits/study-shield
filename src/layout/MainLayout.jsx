2762e2f40073532b08223942f4434d188e6274b9
bt3762334-commits <bt3762334@gmail.com>
2026-06-10 23:46:40 +0300

    Create MainLayout.jsx

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="app-layout">
+      <Sidebar />
+
+      <div className="main-content">
+        <Header />
+
+        <main className="page-content">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
