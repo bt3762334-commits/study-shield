e3f1420e85112cc52ca1c3e3782a85d3adcd5e45
bt3762334-commits <bt3762334@gmail.com>
2026-06-11 18:22:19 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,47 @@ import Lessons from "./pages/Lessons";
 import Lectures from "./pages/Lectures";
 import Achievements from "./pages/Achievements";
 import Settings from "./pages/Settings";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tasks" element={<Tasks />} />
-      <Route path="/lessons" element={<Lessons />} />
-      <Route path="/lectures" element={<Lectures />} />
-      <Route path="/achievements" element={<Achievements />} />
-      <Route path="/settings" element={<Settings />} />
+
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+      <Route
+        path="/tasks"
+        element={<Tasks />}
+      />
+
+      <Route
+        path="/lessons"
+        element={<Lessons />}
+      />
+
+      <Route
+        path="/lectures"
+        element={<Lectures />}
+      />
+
+      <Route
+        path="/achievements"
+        element={<Achievements />}
+      />
+
+      <Route
+        path="/profile"
+        element={<Profile />}
+      />
+
+      <Route
+        path="/settings"
+        element={<Settings />}
+      />
+
     </Routes>
   );
 }
