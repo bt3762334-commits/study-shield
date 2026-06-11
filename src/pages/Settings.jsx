e90a8a82d6902e548e1bd1e773f77c40ec3a4d16
bt3762334-commits <bt3762334@gmail.com>
2026-06-11 17:59:25 +0300

    Update Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,62 @@
 import MainLayout from "../layout/MainLayout";
+import { exportData } from "../services/exportData";
 
 export default function Settings() {
+
+  const resetData = () => {
+
+    if (
+      window.confirm(
+        "هل أنت متأكد من حذف جميع البيانات؟"
+      )
+    ) {
+
+      localStorage.clear();
+
+      window.location.reload();
+    }
+  };
+
   return (
     <MainLayout>
-      <h2>الإعدادات</h2>
+
+      <div className="settings-page">
+
+        <h1>
+          ⚙️ الإعدادات
+        </h1>
+
+        <div className="settings-card">
+
+          <h3>
+            النسخة الاحتياطية
+          </h3>
+
+          <button
+            onClick={exportData}
+          >
+            📤 تصدير البيانات
+          </button>
+
+        </div>
+
+        <div className="settings-card">
+
+          <h3>
+            إعادة ضبط التطبيق
+          </h3>
+
+          <button
+            className="danger-btn"
+            onClick={resetData}
+          >
+            🗑 حذف جميع البيانات
+          </button>
+
+        </div>
+
+      </div>
+
     </MainLayout>
   );
 }
