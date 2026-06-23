1c11329f5d0cc6ea0d457a14f166d359c8e524fc
bt3762334-commits <bt3762334@gmail.com>
2026-06-23 21:27:19 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-import "./styles/globals.css";
-
-// تهيئة الخدمات
-import { themeService } from "./services/themeService";
-import { soundService } from "./services/soundService";
-
-// تهيئة المظهر
-themeService.init();
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(registration => {
-        console.log(
-          "Service Worker registered:",
-          registration
-        );
-      })
-      .catch(error => {
-        console.log(
-          "Service Worker registration failed:",
-          error
-        );
-      });
-  });
-}
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
