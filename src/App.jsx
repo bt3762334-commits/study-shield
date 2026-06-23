c6b1ba343be03fddcd37cecb859a29465ad10a22
bt3762334-commits <bt3762334@gmail.com>
2026-06-23 20:22:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import NotificationManager from "./components/ui/NotificationManager";
 import Pomodoro from "./pages/Pomodoro";
 import Home from "./pages/Home";
 import Tasks from "./pages/Tasks";
-import Lessons from "./pages/Lessons";
 import Lectures from "./pages/Lectures";
 import Achievements from "./pages/Achievements";
 import Settings from "./pages/Settings";
@@ -13,19 +12,20 @@ import Profile from "./pages/Profile";
 
 function AppInner() {
   const { userName } = useUser();
+
   return (
     <>
       <NotificationManager />
       {!userName && <WelcomeModal />}
+
       <Routes>
-        <Route path="/"             element={<Home />} />
-        <Route path="/tasks"        element={<Tasks />} />
-        <Route path="/lessons"      element={<Lessons />} />
-        <Route path="/lectures"     element={<Lectures />} />
-        <Route path="/pomodoro"     element={<Pomodoro />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/lectures" element={<Lectures />} />
+        <Route path="/pomodoro" element={<Pomodoro />} />
         <Route path="/achievements" element={<Achievements />} />
-        <Route path="/profile"      element={<Profile />} />
-        <Route path="/settings"     element={<Settings />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   );
