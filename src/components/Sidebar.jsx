bd3a6eaafbfac2d2fbf8683503adb6eaacf44623
bt3762334-commits <bt3762334@gmail.com>
2026-06-23 21:35:26 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   const link =
-    "block p-3 rounded-lg hover:bg-white/10 transition";
+    "block p-3 rounded-lg hover:bg-white/10 transition text-white";
 
   return (
     <aside className="w-64 bg-[#1e293b] p-4 hidden md:flex flex-col">
-      <h2 className="text-xl font-bold mb-6 text-white">
+      <h2 className="text-xl font-bold mb-6">
         StudyShield
       </h2>
 
