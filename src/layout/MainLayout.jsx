36b4b0465ded62c811ada6ca204566eb41ef7346
bt3762334-commits <bt3762334@gmail.com>
2026-06-23 22:03:36 +0300

    Update MainLayout.jsx

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -13,6 +13,17 @@ const MainLayout = ({ children }) => {
         <main className="container" style={{ padding: "20px" }}>
           {children}
         </main>
+        <footer
+  style={{
+    textAlign: "center",
+    padding: "20px",
+    color: "#9ca3af",
+  }}
+>
+  <p>Dark Byte © 2026</p>
+  <p>باسم طه</p>
+  <p>01091291823</p>
+</footer>
       </div>
 
     </div>
