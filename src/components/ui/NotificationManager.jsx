4f51d481f69b5a1f322ac962c0bcbd35ffeb1505
bt3762334-commits <bt3762334@gmail.com>
2026-06-23 21:15:41 +0300

    Update NotificationManager.jsx

--- src/components/ui/NotificationManager.jsx
+++ src/components/ui/NotificationManager.jsx
@@ -1,16 +1,9 @@
 import { useEffect } from "react";
-import { enableNotifications } from "../../services/notificationService";
 
 export default function NotificationManager() {
   useEffect(() => {
-    if (!("Notification" in window)) return;
-
-    if (Notification.permission === "granted") {
-      enableNotifications();
-
-      if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/service-worker.js").catch(() => {});
-      }
+    if ("Notification" in window) {
+      Notification.requestPermission();
     }
   }, []);
 
